418efb060fa8ecac09bc45e9a64fe41d089bc30f
Admir Trakic <atrakic@users.noreply.github.com>
2023-11-05 17:20:33 +0100

    Use connection string env var

--- dab/bootstrap.sh
+++ dab/bootstrap.sh
@@ -4,8 +4,9 @@ set -eo pipefail
 
 if [ -n "$CONNECTION_STRING" ];
 then
-  rm -rf dab-config.PostgreSql.json
+  rm -rf dab-config.PostgreSql.json || true
   while IFS= read -r line; do
     echo "dab $line" | sh
   done < postgresql-commands.txt
+  mv -f dab-config.PostgreSql.json config.json
 fi
